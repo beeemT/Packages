8ab2e78581e9bda23fdce0358b09d9ec6446cf85
grekhor <benedikt.thoma@gmx.net>
2018-03-03 17:09:40 +0100

    Fixed handleShrink and added a delete without handleShrink

--- queue/removeFunctions.go
+++ queue/removeFunctions.go
@@ -16,16 +16,20 @@ func (q *Queue) remove() *queueElement {
 
 func (q *Queue) handleShrink() {
 	if float64(len(q.queSlice)) < q.shrinkFactor*float64(cap(q.queSlice)) {
-		newLen := int(math.Ceil(q.afterShrinkFactor * float64(len(q.queSlice))))
-		temp := make([]*queueElement, newLen)
+		newCap := int(math.Ceil(q.afterShrinkFactor * float64(cap(q.queSlice))))
+		temp := make([]*queueElement, len(q.queSlice), newCap)
 		copy(temp, q.queSlice[:len(q.queSlice)])
 		q.queSlice = temp
 	}
 }
 
 func (q *Queue) delete(i int) {
+	q.deleteWithoutMemoryManagement(i)
+	q.handleShrink()
+}
+
+func (q *Queue) deleteWithoutMemoryManagement(i int) {
 	copy(q.queSlice[i:], q.queSlice[i+1:])
 	q.queSlice[len(q.queSlice)-1] = nil
 	q.queSlice = q.queSlice[:len(q.queSlice)-1]
-	q.handleShrink()
 }
