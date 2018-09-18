b87361efa9a126906c7d56e2ba1ea627dd54b7b2
Benedikt Thoma <benedikt.thoma@gmx.net>
2018-09-18 13:18:11 +0200

    added a function that computes the parent directory of a path

--- fileutil/paths.go
+++ fileutil/paths.go
@@ -62,6 +62,7 @@ func IsDir(path string) bool {
 
 //IsFile takes a string and returns whether path is a File.
 //Calls PathToAbs on path beforehand.
+//If the file doesn't exist false is returned.
 func IsFile(path string) bool {
 	path, _ = PathToAbs(path)
 	info, err := os.Stat(path)
@@ -71,12 +72,13 @@ func IsFile(path string) bool {
 	return info.Mode().IsRegular()
 }
 
-//IsDirSubdirOf checks if subdir is a subdirectory of dir. Does also check if subdir exists.
+//IsDirSubdirOf checks if subdir is a subdirectory of path. Does also check if subdir exists.
 //Accepts relative paths. Calls PathToAbs beforehand. Returns false if one of the dirs is not a dir.
 func IsDirSubdirOf(subdir, dir string) (bool, error) {
 	if !(IsDir(subdir) && IsDir(dir)) {
 		return false, nil
 	}
+
 	subdir, err := PathToAbs(subdir)
 	dir, err = PathToAbs(dir)
 	if err != nil {
@@ -100,4 +102,20 @@ func Exists(path string) (bool, error) {
 		return false, nil
 	}
 	return false, err
+}
+
+//Parent returns the path to the parent of path.
+//Returns / if path is /.
+func Parent(path string) (string, error) {
+	dir, err := PathToAbs(path)
+	if err != nil {
+		return "", err
+	}
+
+	dir = dir[:strings.LastIndex(dir, "/")]
+	if dir == "" {
+		dir = "/"
+	}
+
+	return dir, nil
 }
